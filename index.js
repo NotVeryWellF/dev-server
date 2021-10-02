58e0d75ea8862627a1b4fe87ece01447795476a1
Ruslan Israfilov <notverywellf@gmail.com>
2021-10-02 07:37:15 +0300

    templates

--- index.js
+++ index.js
@@ -4,12 +4,23 @@ const getModuleData = require('./utils/module-package')
 
 const app = express();
 
+const applyHbs = require('@notverywellc/templates');
+
 const startServer = ({ port }) => {
     const moduleData = getModuleData();
     const appPath = `/${moduleData.name}`;
 
+    applyHbs(app);
+
     app.get(appPath, (req, res) => {
-        res.send(`Hello, ${moduleData.name}`)
+        res.render('index', {
+            baseUrl: '/static',
+            fireAppVersion: "1.0.0",
+            title: "My app",
+            apps: {foo: {version: '1.0.0', name: "foo"}},
+            navigation: {'dummy.main': '/dummy'},
+            config: {}
+        })
     });
 
     app.listen(port, () => {
