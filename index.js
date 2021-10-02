4690fbddb5393cc3b827a6ce22c2c8bb02fe5c28
Ruslan Israfilov <notverywellf@gmail.com>
2021-10-02 08:54:53 +0300

    fireapp

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
 
 const applyHbs = require('@notverywellc/templates');
 
+const baseUrl = "/static"
+
 const startServer = ({ port }) => {
     const moduleData = getModuleData();
     const appPath = `/${moduleData.name}`;
@@ -15,13 +17,23 @@ const startServer = ({ port }) => {
     app.get(appPath, (req, res) => {
         res.render('index', {
             baseUrl: '/static',
-            fireAppVersion: "1.0.0",
+            fireAppVersion: "2.0.0/dist",
             title: "My app",
             apps: {foo: {version: '1.0.0', name: "foo"}},
             navigation: {'dummy.main': '/dummy'},
             config: {}
         })
     });
+
+    app.use(
+		baseUrl,
+        (req, res, next) => {
+            next();
+        },
+		express.Router().get(/\/([.-\w]+)\/([.-\w\d]+)\/(.*)/, (req, res) => {
+            console.log(req.params)
+        })
+    )
 
     app.listen(port, () => {
 		console.log(`server started at: http://localhost:${port}${appPath}`);
